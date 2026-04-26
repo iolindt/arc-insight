d69a98085e3cc14eaedaf976c695bb736ae5b230
iolindt <ramazzanov@gmail.com>
2026-04-26 14:19:21 +0300

    Update arcController.js

--- src/controllers/arcController.js
+++ src/controllers/arcController.js
@@ -1,1 +1,24 @@
+const arcService = require("../services/arcService");
+const logger = require("../utils/logger");
 
+exports.getBlocks = async (req, res, next) => {
+  try {
+    const data = await arcService.fetchBlocks();
+    res.json({ success: true, data });
+  } catch (err) {
+    logger.error(err);
+    next(err);
+  }
+};
+
+exports.getWallet = async (req, res, next) => {
+  try {
+    const { address } = req.params;
+    const data = await arcService.fetchWallet(address);
+
+    res.json({ success: true, data });
+  } catch (err) {
+    logger.error(err);
+    next(err);
+  }
+};
