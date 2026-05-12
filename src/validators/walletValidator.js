4094fe44ae8e5da76519b4a3277b1ee517589737
iolindt <ramazzanov@gmail.com>
2026-05-12 14:32:40 +0300

    Update walletValidator.js

--- src/validators/walletValidator.js
+++ src/validators/walletValidator.js
@@ -1,1 +1,12 @@
+exports.validateWallet = (req, res, next) => {
+  const { address } = req.params;
 
+  if (!address || address.length < 10) {
+    return res.status(400).json({
+      success: false,
+      error: "Invalid wallet address"
+    });
+  }
+
+  next();
+};
