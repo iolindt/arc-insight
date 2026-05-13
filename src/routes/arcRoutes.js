2d695a864a2afedf81b728e309ec884ccd05f820
iolindt <ramazzanov@gmail.com>
2026-05-13 14:53:33 +0300

    Update arcRoutes.js

--- src/routes/arcRoutes.js
+++ src/routes/arcRoutes.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
+
 const controller = require("../controllers/arcController");
+const { validateWallet } = require("../validators/walletValidator");
 
 router.get("/blocks", controller.getBlocks);
-router.get("/wallet/:address", controller.getWallet);
+
+router.get(
+  "/wallet/:address",
+  validateWallet,
+  controller.getWallet
+);
 
 module.exports = router;
