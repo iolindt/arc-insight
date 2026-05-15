900dc3f51d81646726be44c27c3c36c199843514
iolindt <ramazzanov@gmail.com>
2026-05-15 12:42:40 +0300

    Update healthController.js

--- src/controllers/healthController.js
+++ src/controllers/healthController.js
@@ -1,1 +1,8 @@
-
+exports.healthCheck = (req, res) => {
+  res.status(200).json({
+    success: true,
+    uptime: process.uptime(),
+    message: "API is healthy",
+    timestamp: Date.now()
+  });
+};
