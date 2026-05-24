553099144b49baeb0bd447ba10421ac0616e8a7e
iolindt <ramazzanov@gmail.com>
2026-05-24 17:11:49 +0300

    Update cacheService.js

--- services/cacheService.js
+++ services/cacheService.js
@@ -1,1 +1,15 @@
+class CacheService {
+  constructor() {
+    this.cache = new Map();
+  }
 
+  get(key) {
+    return this.cache.get(key);
+  }
+
+  set(key, value) {
+    this.cache.set(key, value);
+  }
+}
+
+module.exports = new CacheService();
