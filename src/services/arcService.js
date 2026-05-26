2d1ebc606d9b921ff5ceeb1fccc83ef7e2ff2d66
iolindt <ramazzanov@gmail.com>
2026-05-26 16:46:20 +0300

    Create arcService.js

---
+++ src/services/arcService.js
@@ -0,0 +1,13 @@
+class CacheService {
+  constructor() {
+    this.cache = new Map();
+  }
+
+  get(key) {
+    return this.cache.get(key);
+  }
+
+  set(key, value) {
+    this.cache.set(key, value);
+  }
+}
