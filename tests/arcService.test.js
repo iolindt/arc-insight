57458775178e1a01cc4afadb3a05122edf132cd8
iolindt <ramazzanov@gmail.com>
2026-06-14 14:52:27 +0300

    Update arcService.test.js

--- tests/arcService.test.js
+++ tests/arcService.test.js
@@ -1,1 +1,17 @@
+const cacheService = require("../src/services/cacheService");
 
+describe("CacheService", () => {
+  test("should store and retrieve value", () => {
+    cacheService.set("wallet", "0x123");
+
+    expect(
+      cacheService.get("wallet")
+    ).toBe("0x123");
+  });
+
+  test("should return undefined for missing key", () => {
+    expect(
+      cacheService.get("missing")
+    ).toBeUndefined();
+  });
+});
