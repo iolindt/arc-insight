00cbb0d0cda2017a975370e4832eb18845bad14a
iolindt <ramazzanov@gmail.com>
2026-06-20 11:39:30 +0300

    Update swagger.js

--- docs/swagger.js
+++ docs/swagger.js
@@ -1,1 +1,24 @@
+const swaggerJsDoc = require("swagger-jsdoc");
 
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Arc Insight API",
+      version: "1.0.0",
+      description: "Analytics API for Arc Network"
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+        description: "Development Server"
+      }
+    ]
+  },
+  apis: [
+    "./src/routes/*.js",
+    "./src/controllers/*.js"
+  ]
+};
+
+module.exports = swaggerJsDoc(options);
