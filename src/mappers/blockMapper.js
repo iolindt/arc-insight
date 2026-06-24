002edae93e474a1e6247dd48dd86ce38ee92e2c4
iolindt <ramazzanov@gmail.com>
2026-06-24 14:22:34 +0300

    Update blockMapper.js

--- src/mappers/blockMapper.js
+++ src/mappers/blockMapper.js
@@ -1,1 +1,16 @@
+class BlockMapper {
+  static toResponse(block) {
+    return {
+      id: block.id,
+      txCount: block.transactions?.length || 0,
+      timestamp: block.timestamp,
+      validator: block.validator || null
+    };
+  }
 
+  static toCollection(blocks) {
+    return blocks.map(block => this.toResponse(block));
+  }
+}
+
+module.exports = BlockMapper;
