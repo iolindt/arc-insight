0543dee686ac04690aa3e96d89b60691fa2be09e
iolindt <ramazzanov@gmail.com>
2026-06-25 16:37:20 +0300

    Update walletMapper.js

--- src/mappers/walletMapper.js
+++ src/mappers/walletMapper.js
@@ -1,1 +1,14 @@
+class WalletMapper {
+  static toResponse(address, walletData) {
+    const txCount = walletData.transactions?.length || 0;
 
+    return {
+      address,
+      txCount,
+      isActive: txCount > 10,
+      lastActivity: walletData.transactions?.[0]?.timestamp || null
+    };
+  }
+}
+
+module.exports = WalletMapper;
